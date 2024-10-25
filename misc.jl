d6ba15c4339e108fff38f6334a4f72231d72893c
datejada <diego.tejadaarango@tno.nl>
2024-10-25 11:45:00 +0200

    Add user files for yearly data

--- misc.jl
+++ misc.jl
@@ -34,3 +34,18 @@ CSV.write("user-input-files/assets-conversion-basic-data.csv", df_conversion)
 CSV.write("user-input-files/assets-storage-basic-data.csv", df_storage)
 
 # create assets files (year dependent)
+df_year = CSV.read(joinpath(input_dir, "assets-data.csv"), DataFrame, header=2)
+leftjoin!(df_year, df[:, [:name, :type]], on=:name)
+
+df_hub_year = df_year[df_year.type.=="hub", [:name, :year]]
+df_consumer_year = df_year[df_year.type.=="consumer", [:name, :year, :peak_demand]]
+df_producer_year = df_year[df_year.type.=="producer", [:name, :year, :initial_units, :unit_commitment, :unit_commitment_method, :units_on_cost, :unit_commitment_integer, :min_operating_point, :ramping, :max_ramp_up, :max_ramp_down]]
+df_conversion_year = df_year[df_year.type.=="conversion", [:name, :year, :initial_units, :unit_commitment, :unit_commitment_method, :units_on_cost, :unit_commitment_integer, :min_operating_point, :ramping, :max_ramp_up, :max_ramp_down]]
+df_storage_year = df_year[df_year.type.=="storage", [:name, :year, :initial_units, :initial_storage_units, :initial_storage_level, :storage_inflows]]
+
+CSV.write("user-input-files/assets-hub-yearly-data.csv", df_hub_year)
+CSV.write("user-input-files/assets-consumer-yearly-data.csv", df_consumer_year)
+CSV.write("user-input-files/assets-producer-yearly-data.csv", df_producer_year)
+CSV.write("user-input-files/assets-conversion-yearly-data.csv", df_conversion_year)
+CSV.write("user-input-files/assets-storage-yearly-data.csv", df_storage_year)
+
