7db46c1c0db55de16f4af3930471dfd54ecce7d7
Diego Alejandro Tejada Arango <diego.tejadaarango@tno.nl>
2025-01-29 15:47:03 +0100

    Update julia files to create v0.11 files

--- utils/preprocess-profiles.jl
+++ utils/preprocess-profiles.jl
@@ -7,7 +7,8 @@ input_min_max_reservoir_profiles_file =
 output_profiles_file = joinpath(@__DIR__, "..", tulipa_files_dir, "profiles-rep-periods.csv")
 output_mapping_file = joinpath(@__DIR__, "..", tulipa_files_dir, "rep-periods-mapping.csv")
 output_rp_file = joinpath(@__DIR__, "..", tulipa_files_dir, "rep-periods-data.csv")
-output_timeframe_profiles_file = joinpath(@__DIR__, "..", tulipa_files_dir, "profiles-timeframe.csv")
+output_timeframe_profiles_file =
+    joinpath(@__DIR__, "..", tulipa_files_dir, "profiles-timeframe.csv")
 output_timeframe_data_file = joinpath(@__DIR__, "..", tulipa_files_dir, "timeframe-data.csv")
 
 # Read the input profiles
@@ -44,19 +45,13 @@ TulipaClustering.fit_rep_period_weights!(
 clustered_profiles = clustering_result.profiles
 clustered_profiles =
     select(clustered_profiles, [:profile_name, :year, :rep_period, :timestep, :value])
-open(output_profiles_file, "w") do io
-    println(io, join(["", "", "", "", "p.u."], ","))
-end
-CSV.write(output_profiles_file, clustered_profiles; append = true, writeheader = true)
+CSV.write(output_profiles_file, clustered_profiles; append = false, writeheader = true)
 
 # Save the mapping data
 mapping = TulipaClustering.weight_matrix_to_df(clustering_result.weight_matrix)
 mapping.year .= default_values["year"]
 mapping = select(mapping, [:year, :period, :rep_period, :weight])
-open(output_mapping_file, "w") do io
-    println(io, join(["", "", "", "p.u."], ","))
-end
-CSV.write(output_mapping_file, mapping; append = true, writeheader = true)
+CSV.write(output_mapping_file, mapping; append = false, writeheader = true)
 
 # Save the representative period data
 first_clustered_profile =
@@ -66,10 +61,16 @@ rp_data = combine(
     :timestep => length => :num_timesteps,
 )
 rp_data.resolution .= 1.0
-open(output_rp_file, "w") do io
-    println(io, join(["", "", "", ""], ","))
-end
-CSV.write(output_rp_file, rp_data; append = true, writeheader = true)
+CSV.write(output_rp_file, rp_data; append = false, writeheader = true)
+
+# save timeframe data file
+first_profile = filter(
+    row -> row.profile_name == first(profiles_long_format.profile_name),
+    profiles_long_format,
+)
+timeframe_data =
+    combine(groupby(first_profile, [:year, :period]), :timestep => length => :num_timesteps)
+CSV.write(output_timeframe_data_file, timeframe_data; append = false, writeheader = true)
 
 # Write extra files for n_rp > 1
 if n_rp > 1
@@ -79,23 +80,10 @@ if n_rp > 1
         groupby(min_max_reservoir_profiles_long_format, [:profile_name, :year, :period]),
         :value => Statistics.mean => :value,
     )
-    open(output_timeframe_profiles_file, "w") do io
-        println(io, join(["", "", "", "p.u."], ","))
-    end
     CSV.write(
         output_timeframe_profiles_file,
         grouped_min_max_reservoir_profiles_long_format;
-        append = true,
+        append = false,
         writeheader = true,
     )
-    first_profile = filter(
-        row -> row.profile_name == first(profiles_long_format.profile_name),
-        profiles_long_format,
-    )
-    timeframe_data =
-        combine(groupby(first_profile, [:year, :period]), :timestep => length => :num_timesteps)
-    open(output_timeframe_data_file, "w") do io
-        println(io, join(["", "", ""], ","))
-    end
-    CSV.write(output_timeframe_data_file, timeframe_data; append = true, writeheader = true)
 end
--- utils/preprocess-user-inputs.jl
+++ utils/preprocess-user-inputs.jl
@@ -1,33 +1,56 @@
-## write graph assets data file
-tulipa_file = "graph-assets-data.csv"
+## write asset-both data file
+tulipa_file = "asset-both.csv"
 process_user_files(
     joinpath(@__DIR__, "..", user_input_dir),
     joinpath(@__DIR__, "..", tulipa_files_dir, tulipa_file),
-    TulipaEnergyModel.schemas.graph.assets,
-    "assets",
-    "basic-data.csv",
-    default_values,
-)
-
-## write assets data file
-tulipa_file = "assets-data.csv"
-assets_data = process_user_files(
-    joinpath(@__DIR__, "..", user_input_dir),
-    joinpath(@__DIR__, "..", tulipa_files_dir, tulipa_file),
-    TulipaEnergyModel.schemas.assets.data,
+    TulipaEnergyModel.schemas.assets.both,
     "assets",
     "yearly-data.csv",
-    default_values,
+    default_values;
+    map_to_rename_user_columns = Dict("name" => "asset"),
+)
+
+## write asset-commission file
+tulipa_file = "asset-commission.csv"
+process_user_files(
+    joinpath(@__DIR__, "..", user_input_dir),
+    joinpath(@__DIR__, "..", tulipa_files_dir, tulipa_file),
+    TulipaEnergyModel.schemas.assets.commission,
+    "assets",
+    "yearly-data.csv",
+    default_values;
+    map_to_rename_user_columns = Dict("name" => "asset"),
+)
+
+## write asset-milestone file
+tulipa_file = "asset-milestone.csv"
+process_user_files(
+    joinpath(@__DIR__, "..", user_input_dir),
+    joinpath(@__DIR__, "..", tulipa_files_dir, tulipa_file),
+    TulipaEnergyModel.schemas.assets.milestone,
+    "assets",
+    "yearly-data.csv",
+    default_values;
+    map_to_rename_user_columns = Dict("name" => "asset"),
+)
+
+## write asset file
+tulipa_file = "asset.csv"
+assets = process_user_files(
+    joinpath(@__DIR__, "..", user_input_dir),
+    joinpath(@__DIR__, "..", tulipa_files_dir, tulipa_file),
+    TulipaEnergyModel.schemas.assets.basic,
+    "assets",
+    "basic-data.csv",
+    default_values;
+    map_to_rename_user_columns = Dict("name" => "asset"),
 )
 
 ## if n_rp = 1 (full-year optimization) update is_seasonal to false
 if n_rp == 1
-    assets_data.is_seasonal .= false
+    assets.is_seasonal .= false
     output_file = joinpath(@__DIR__, "..", tulipa_files_dir, tulipa_file)
-    open(output_file, "w") do io
-        println(io, repeat(",", size(assets_data, 2) - 1))
-    end
-    CSV.write(output_file, assets_data; append = true, writeheader = true)
+    CSV.write(output_file, assets; append = false, writeheader = true)
 end
 
 ## write assets-profiles data file
@@ -38,17 +61,6 @@ process_user_files(
     TulipaEnergyModel.schemas.assets.profiles_reference,
     "assets",
     "profiles.csv",
-    default_values,
-)
-
-## write assets-timeframe-profiles.csv file
-tulipa_file = "assets-timeframe-profiles.csv"
-process_user_files(
-    joinpath(@__DIR__, "..", user_input_dir),
-    joinpath(@__DIR__, "..", tulipa_files_dir, tulipa_file),
-    TulipaEnergyModel.schemas.assets.profiles_reference,
-    "assets",
-    "min-max-reservoir-level-profiles.csv",
     default_values,
 )
 
@@ -65,25 +77,58 @@ process_user_files(
     number_of_rep_periods = n_rp,
 )
 
-## write graph flows data file
-tulipa_file = "graph-flows-data.csv"
+## write assets-timeframe-profiles.csv file
+tulipa_file = "assets-timeframe-profiles.csv"
 process_user_files(
     joinpath(@__DIR__, "..", user_input_dir),
     joinpath(@__DIR__, "..", tulipa_files_dir, tulipa_file),
-    TulipaEnergyModel.schemas.graph.flows,
-    "flows",
-    "basic-data.csv",
+    TulipaEnergyModel.schemas.assets.profiles_reference,
+    "assets",
+    "min-max-reservoir-level-profiles.csv",
     default_values,
 )
 
-## write flow data file
-tulipa_file = "flows-data.csv"
+## write flow both file
+tulipa_file = "flow-both.csv"
 process_user_files(
     joinpath(@__DIR__, "..", user_input_dir),
     joinpath(@__DIR__, "..", tulipa_files_dir, tulipa_file),
-    TulipaEnergyModel.schemas.flows.data,
+    TulipaEnergyModel.schemas.flows.both,
     "flows",
     "yearly-data.csv",
+    default_values,
+)
+
+## write flow commission file
+tulipa_file = "flow-commission.csv"
+process_user_files(
+    joinpath(@__DIR__, "..", user_input_dir),
+    joinpath(@__DIR__, "..", tulipa_files_dir, tulipa_file),
+    TulipaEnergyModel.schemas.flows.commission,
+    "flows",
+    "yearly-data.csv",
+    default_values,
+)
+
+## write flow milestone file
+tulipa_file = "flow-milestone.csv"
+process_user_files(
+    joinpath(@__DIR__, "..", user_input_dir),
+    joinpath(@__DIR__, "..", tulipa_files_dir, tulipa_file),
+    TulipaEnergyModel.schemas.flows.milestone,
+    "flows",
+    "yearly-data.csv",
+    default_values,
+)
+
+## write flow file
+tulipa_file = "flow.csv"
+process_user_files(
+    joinpath(@__DIR__, "..", user_input_dir),
+    joinpath(@__DIR__, "..", tulipa_files_dir, tulipa_file),
+    TulipaEnergyModel.schemas.flows.basic,
+    "flows",
+    "basic-data.csv",
     default_values,
 )
 
@@ -95,28 +140,6 @@ process_user_files(
     TulipaEnergyModel.schemas.flows.profiles_reference,
     "flows",
     "profiles.csv",
-    default_values,
-)
-
-## write vintage assets data file
-tulipa_file = "vintage-assets-data.csv"
-process_user_files(
-    joinpath(@__DIR__, "..", user_input_dir),
-    joinpath(@__DIR__, "..", tulipa_files_dir, tulipa_file),
-    TulipaEnergyModel.schemas.assets.vintage_assets_data,
-    "assets",
-    "basic-data.csv",
-    default_values,
-)
-
-## write vintage flows data file
-tulipa_file = "vintage-flows-data.csv"
-process_user_files(
-    joinpath(@__DIR__, "..", user_input_dir),
-    joinpath(@__DIR__, "..", tulipa_files_dir, tulipa_file),
-    TulipaEnergyModel.schemas.assets.vintage_flows_data,
-    "flows-transport",
-    "basic-data.csv",
     default_values,
 )
 
@@ -135,7 +158,7 @@ process_user_files(
 tulipa_file = "flows-rep-periods-partitions.csv"
 process_flows_rep_period_partition_file(
     joinpath(@__DIR__, "..", tulipa_files_dir, "assets-rep-periods-partitions.csv"),
-    joinpath(@__DIR__, "..", tulipa_files_dir, "flows-data.csv"),
+    joinpath(@__DIR__, "..", tulipa_files_dir, "flow.csv"),
     joinpath(@__DIR__, "..", tulipa_files_dir, tulipa_file),
     TulipaEnergyModel.schemas.flows.rep_periods_partition,
     default_values,
